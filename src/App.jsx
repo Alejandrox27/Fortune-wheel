0ef52dbf54c85b9a260993d379b1bedd5a0f1b78
alx_27 <didieralejandromejia83@gmail.com>
2024-01-10 19:41:48 -0500

    Add animation to bar

--- src/App.jsx
+++ src/App.jsx
@@ -2,8 +2,12 @@ import wheel from "./imgs/ruleta.png"
 import arrow from "./imgs/central.png"
 import "./App.css"
 import "./reset.css"
+import { useState } from "react"
 
 function App(){
+
+  const [len, setLen] = useState(240)
+
   return(
     <div className="container">
       <div className="wheel-container">
@@ -14,7 +18,10 @@ function App(){
         <p>Click on "launch"</p>
       </div>
       <div className="launch-container">
-        <div className="launch-bar"></div>
+        <div className="launch-bar animation-bar"
+        style={{
+          "--len": `${len}px` 
+        }}></div>
       </div>
       <div>
         <button className="launch-button">Launch</button>
