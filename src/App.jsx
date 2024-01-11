8889a75a1ab06d5140e65690cb1deb2b188dd2ca
alx_27 <didieralejandromejia83@gmail.com>
2024-01-10 19:28:53 -0500

    Create the styles for the wheel

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,24 @@
 import wheel from "./imgs/ruleta.png"
 import arrow from "./imgs/central.png"
 import "./App.css"
+import "./reset.css"
 
 function App(){
   return(
-    <div className="wheel-container">
-      <img className="wheel animation" src={wheel}></img>
-      <img className="arrow" src={arrow}></img>
+    <div className="container">
+      <div className="wheel-container">
+        <img className="wheel animation" src={wheel}></img>
+        <img className="arrow" src={arrow}></img>
+      </div>
+      <div className="launch-text">
+        <p>Click on "launch"</p>
+      </div>
+      <div className="launch-container">
+        <div className="launch-bar"></div>
+      </div>
+      <div>
+        <button className="launch-button">Launch</button>
+      </div>
     </div>
   )
 }
