395bcde07b88ba02e4d88d44868a63f8d71d9336
alx_27 <didieralejandromejia83@gmail.com>
2024-01-10 20:18:01 -0500

    Transform for rotation

--- src/App.jsx
+++ src/App.jsx
@@ -3,18 +3,28 @@ import arrow from "./imgs/central.png"
 import "./App.css"
 import "./reset.css"
 import { useRef } from "react"
+import { useState } from "react"
 
 function App(){
   const barRef = useRef(null);
+  const wheelRef = useRef(null);
+
+  const [power, setPower] = useState(10)
 
   const stop = () => {
     barRef.current.classList.toggle("stop");
+    setPower(barRef.current.offsetWidth * 10);
   }
 
   return(
     <div className="container">
       <div className="wheel-container">
-        <img className="wheel animation" src={wheel}></img>
+        <img className="wheel" style={{
+          "--power": power,
+          transform: `rotate(${power}deg)`,
+          transition: "transform 3s cubic-bezier(0.2,0.8,0.7,0.99)"
+        }} src={wheel}
+        ref={ wheelRef }></img>
         <img className="arrow" src={arrow}></img>
       </div>
       <div className="launch-text">
@@ -23,7 +33,7 @@ function App(){
       <div className="launch-container">
         <div className="launch-bar animation-bar"
         style={{
-          "--len": `240px` 
+          "--len": `240px`,
         }}
         ref={ barRef }></div>
       </div>
